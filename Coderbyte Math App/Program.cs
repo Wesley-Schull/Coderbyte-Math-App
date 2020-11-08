8bcb79830b6375dce3ad09cba49a3429a626a551
Wesley Schull <varnathin@gmail.com>
2020-11-08 13:15:56 -0600

    The main working project branch.

---
+++ Coderbyte Math App/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Json.Net;
+
+namespace Coderbyte_Math_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

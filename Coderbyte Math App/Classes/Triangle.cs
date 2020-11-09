217a489066644e75c1712c7bc0a30ca22d76658c
Wesley Schull <varnathin@gmail.com>
2020-11-08 20:41:38 -0600

    Added Triangle class, uses Heron's Formula to calculate surface area

---
+++ Coderbyte Math App/Classes/Triangle.cs
@@ -0,0 +1,49 @@
+﻿using Coderbyte_Math_App.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Coderbyte_Math_App.Classes
+{
+    class Triangle : IShape
+    {
+        public string Name { get; set; }
+        public double Perimeter { get; set; }
+        public double SurfaceArea { get; set; }
+        public string TriangleType { get; set; } // TODO: implement check so only "Equilateral", "Isosceles", and "Scalene" are accepted
+        public double SideA { get; set; }
+        public double SideB { get; set; }
+        public double SideC { get; set; }
+
+        public void CalculatePerimeter()
+        {
+            Perimeter = SideA + SideB + SideC;
+        }
+
+        public void CalculateSurfaceArea()
+        {
+            /** Heron's Formula:
+             *  s = Perimeter / 2
+             *  Area = sqrt(s(s-a)(s-b)(s-c))
+             */
+            if (Perimeter == 0) CalculatePerimeter();
+            
+            double s = Perimeter / 2;
+
+            SurfaceArea = Math.Sqrt(s * (s - SideA) * (s - SideB) * (s - SideC));
+        }
+
+        public Triangle(string name,  string type, double a, double b, double c)
+        {
+            Name = name;
+            TriangleType = type;
+            SideA = a;
+            SideB = b;
+            SideC = c;
+
+            CalculatePerimeter();
+            CalculateSurfaceArea();
+        }
+    }
+}

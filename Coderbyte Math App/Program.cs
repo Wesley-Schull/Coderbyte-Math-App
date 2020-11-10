6abda2f373074b69bbb9ddecc3dcf46a4155b3cf
Wesley Schull <varnathin@gmail.com>
2020-11-09 21:47:52 -0600

    I spent a few hours going through tutorials and walkthroughs to familiarize myself with serialization and logging. Still a little shakey, but I think I can manage.

--- Coderbyte Math App/Program.cs
+++ Coderbyte Math App/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.IO;
 using Coderbyte_Math_App.Classes;
 using Json.Net;
+using Microsoft.Extensions.Configuration;
+using Serilog;
 
 namespace Coderbyte_Math_App
 {
@@ -8,7 +11,22 @@ namespace Coderbyte_Math_App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Coderbyte Math App!");
+            var builder = new ConfigurationBuilder();
+            BuildConfig(builder);
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Build())
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+        }
+
+        static void BuildConfig(IConfigurationBuilder builder)
+        {
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+                .AddEnvironmentVariables();
         }
     }
 }
